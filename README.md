5c044591a6b79b4203d7b3c634ff02f03bdc61c4
Joe Chandler <jchandler@partners.cmtelematics.com>
2025-04-25 15:27:48 -0700

    Update dotfiles with latest changes

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # dotfiles
 Personal dotfile repository for local machine configuration.
+
+## Usage
+
+Simply run `./install` and enjoy!
--- pre-install.sh
+++ pre-install.sh
@@ -7,6 +7,12 @@ if [ ! -d "$OMZDIR" ]; then
   /bin/sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 fi
 
+# Install brew if it does not exist
+if ! command -v brew &> /dev/null; then
+  echo 'Installing brew'
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+
 # # Change default shell
 # if [ ! $0 = "-zsh" ]; then
 #   echo 'Changing default shell to zsh'
